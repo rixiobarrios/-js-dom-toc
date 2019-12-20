d7bfaf581b734e32861fb91446880bea94e761da
rixiobarrios <rixiobarrios.consulado@gmail.com>
2019-12-19 23:34:23 -0600

    Rixio Barrios js-dom-toc

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-// generate your Table of Contents using DOM manipulation
+const table = document.createElement('h1');
+const mainHeader = document.getElementsByTagName('h1');
+
+const headerText = 'Table of Contents';
+table.innerHTML = headerText;
+
+document.getElementById('table-of-contents').appendChild(table);
+
+const ul = document.createElement('ul');
+document.getElementById('table-of-contents').appendChild(ul);
+
+const headings = document.getElementsByClassName('heading');
+
+for (var i = 0; i < headings.length; i++) {
+    const li = document.createElement('li');
+    li.innerText = headings[i].innerText;
+    ul.appendChild(li);
+}
